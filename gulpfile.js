ed7694b537690de9c46f4c302f4e8d773f109cdc
ksmskm <ksmarvich@gmail.com>
2017-07-26 18:50:24 -0700

    done, souping up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ const livereload = require('gulp-livereload');
 
 gulp.task('watch', function () {
   livereload.listen();
-  gulp.watch('index.html', ['html']);
+  // gulp.watch('*.html', ['html']);
   gulp.watch('./assets/stylesheets/main.css', ['css']);
 });
 
 gulp.task('default', ['watch']);
 
 gulp.task('html', function() {
-  gulp.src('index.html').pipe(livereload());
+  gulp.src('*.html').pipe(livereload());
 });
 
 gulp.task('css', function() {
-  gulp.src('./assets/stylesheets/main.css').pipe(livereload());
+  gulp.src('./assets/stylesheets/*.css').pipe(livereload());
 });
