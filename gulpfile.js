63e613ad6bf8a651e66b937b00c58e6d3167a173
ksmskm <ksmarvich@gmail.com>
2017-07-28 20:50:31 -0700

    initial success with gulp scss, solved chrome cache issue with cache killer extension

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,61 @@
-const gulp       = require('gulp');
-const livereload = require('gulp-livereload');
+var gulp = require('gulp');
+var beep = require('beepbeep');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglifyjs');
+// var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
-gulp.task('watch', function () {
-  livereload.listen();
-  // gulp.watch('*.html', ['html']);
-  gulp.watch('./assets/stylesheets/main.css', ['css']);
+var onError = function (err) {
+  beep([0, 0, 0]);
+  gutil.log(gutil.colors.green(err));
+};
+
+// JS
+gulp.task('uglifyjs', function() {
+  return gulp.src([
+    // './node_modules/jquery/dist/jquery.min.js',
+    './js/main.js'
+  ])
+  .pipe(plumber({
+    errorHandler: onError
+  }))
+  .pipe(uglify('app.js', {
+    compress: false
+  }))
+  .pipe(gulp.dest('./js/'))
+  .pipe(livereload());
 });
 
-gulp.task('default', ['watch']);
+// SASS
+gulp.task('sass', function() {
+  return gulp.src([
+    './scss/app.scss'
+  ])
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css'))
+  .pipe(livereload());
+});
 
+// HTML
 gulp.task('html', function() {
-  gulp.src('*.html').pipe(livereload());
+  return gulp.src([
+    './*.html',
+    './css/app.css'
+  ])
+  .pipe(livereload());
 });
 
-gulp.task('css', function() {
-  gulp.src('./assets/stylesheets/*.css').pipe(livereload());
+// PRIMARY WATCHER
+gulp.task('default', function() {
+  livereload.listen();
+  // gulp.watch('./js/main.js', ['uglifyjs']);
+  gulp.watch(['./scss/_mixins.scss', './scss/_styles.scss', './scss/app.scss'], ['sass']);
+  gulp.watch('./*.html', ['html']);
+})
+
+gulp.task('build', function() {
+  gulp.start('uglifyjs', 'sass');
 });
